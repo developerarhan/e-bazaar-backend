3a0da05a47f862fbc38b117594eaf3df61f06ecd
developerarhan <khanarhan0205@gmail.com>
2026-07-22 00:39:08 +0530

    fix: add settings in store/views.py

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 import logging
 from django.shortcuts import get_object_or_404
 from django.core.cache import cache
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
